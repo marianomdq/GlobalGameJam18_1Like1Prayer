b844e5c0a5e4e9b0065a83db0975804ebba9f4d0
TomasGimenezR <tomasgimenezr@gmail.com>
2018-01-27 12:07:45 -0300

    Camara apuntando Jugador

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed;
+    }
+}
