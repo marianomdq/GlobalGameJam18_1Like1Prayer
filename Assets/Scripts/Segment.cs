3970bbd68adde9e5be4c24fa4aaae4f8959cea61
MarianoMDQ-StealthPro <marianomdq05@gmail.com>
2018-01-27 10:03:43 -0300

    First Steps - Spawner + Segments + Materials

---
+++ Assets/Scripts/Segment.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Segment : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] Segments;
+    public GameObject CurrentSegment;
+
+    public static Spawner instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Spawn()
+    {
+        Vector3 SpawningPosition = CurrentSegment.transform.position;
+        Quaternion SpawningRotation = CurrentSegment.transform.rotation;
+        int SegmentIndex = Random.Range(0, Segments.Length-1);
+
+        Instantiate(Segments[SegmentIndex], SpawningPosition, SpawningRotation);
+
+
+
+
+    }
+}
