0dd20d912167b61b164cbedc19a369a032e34bb5
Mer Moyano <moyanomariamercedes@gmail.com>
2018-01-27 22:45:09 -0300

    Camera Follow (no lerp yet)

---
+++ Assets/Scripts/FollowLerp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowLerp : MonoBehaviour {
+
+    public GameObject player;
+    public float speed = 2.0f;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+
+        //transform.LookAt(player.transform.parent);
+    }
+}
